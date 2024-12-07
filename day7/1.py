949ee3bbcfb5aa546d9e7f2de859ad7c15197d61
Gian <flaconumero2@gmail.com>
2024-12-07 02:25:23 -0500

    Add day 7 solutions

---
+++ day7/1.py
@@ -0,0 +1,22 @@
+from re import match
+from functools import cache
+file = open('../input','r')
+
+def possible(n, arr):
+
+    def test(i, value):
+        if i >= len(arr):
+            return value == n
+        return test(i+1, arr[i] * value) or test(i+1, arr[i] + value)
+
+    return test(1, arr[0])
+
+ans = 0
+for line in file:
+    line = line.strip()
+    line = match(r'(\d+): (.+)', line)
+    n = int(line.group(1))
+    arr = list(map(int,line.group(2).split()))
+    if possible(n,arr): ans += n
+
+print(ans)
